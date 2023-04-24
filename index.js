47d05a3e4f7f362c479e385c1ea9adef73fe5c51
Stimarc <rikudo7474@gmail.net>
2023-04-24 19:01:51 +0300

    recursion,update  19.04 17:00

--- index.js
+++ index.js
@@ -1,62 +1,105 @@
-// 1.
-
-let admin, name;
-
-name = "Джон";
-
-admin = name;
-
-alert(admin);
-
-// 2.
-
-const planetName = "Ерта";
-
-console.info("Название планеты:", planetName);
-
-// 3.
-let variable1 = prompt("Введите значения 1:");
-let variable2 = prompt("Введите значения 2:");
-let variable3 = prompt("Введите значения 3:");
-
-console.warn(variable1);
-console.warn(variable2);
-console.warn(variable3);
-
-// 4.
-
-const Name = prompt("Как вас зовут?");
-const yearOfBirth = prompt("Сколько вам лет?");
-
-
-const yearOfBirthNumber = parseInt(yearOfBirth);
-if (isNaN(yearOfBirthNumber)) {
-  console.log("Вы ввели неправильную дату рождения.");
- 
-  throw new Error("Ошибка: неправильный год рождения.");
+//Рекурсія:
+//1
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
-
-
-const currentYear = new Date().getFullYear();
-const age = currentYear - yearOfBirthNumber;
-
-
-if (age >= 18) {
-  console.log(`${Name}, вам ${age} лет. Ви можете гулять!`);
-} else {
-  console.log(`${Name}, к сожелению, ви еще не достигли совершенолетия. Гулять запрещено!`);
+//2
+function isPalindrome(word) {
+  word = word.toLowerCase();
+  
+  if (word.length <= 1) {
+    return true;
+  } else if (word[0] !== word[word.length - 1]) {
+    return false;
+  } else {
+    return isPalindrome(word.slice(1, word.length - 1));
+  }
 }
-
-
-// .5
-
-const input = prompt("Введите число:");
-const number = Number(input);
-
-if (number >= 10 && number <= 1100) {
-  console.log(`${number} входит в диапазон от 10 до 1100.`);
-} else {
-  console.log(`${number} не входит в диапазон от 10 до 1100.`);
+//3
+function Recursion(a, b) {
+  if (b === 0) {
+    return a;
+  } else {
+    return Recursion(b, a % b);
+  }
 }
+//Робота з масивами:
+//1
+function increaseValues(arr) {
+  const sum = arr.reduce((acc, val) => acc + val, 0);
+  const avg = sum / arr.length;
+  const result = Array(arr.length);
 
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = arr[i] < avg ? arr[i] + 1 : arr[i];
+  }
 
+  return result;
+}
+//2
+function findLongestWord(sentence) {
+  const words = sentence.split(' ');
+  let longestWord = '';
+  let longestWordLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    if (word.length > longestWordLength) {
+      longestWord = word;
+      longestWordLength = word.length;
+    }
+  }
+
+  return { word: longestWord, length: longestWordLength };
+}
+//3
+function numbersToLetters(numbers) {
+  const letters = [];
+  for (let i = 0; i < numbers.length; i++) {
+    const letter = String.fromCharCode(64 + parseInt(numbers[i]));
+    letters.push(letter);
+  }
+  return letters;
+}
+//4
+function filterDuplicates(arr) {
+  const uniqueArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (uniqueArr.indexOf(arr[i]) === -1) {
+      uniqueArr.push(arr[i]);
+    }
+  }
+  return uniqueArr;
+}
+//5
+function createArray(n, first = 0, second = 1) {
+  const result = [first, second];
+
+  for (let i = 2; i < n; i++) {
+    const sum = result[i - 1] + result[i - 2];
+    result.push(sum);
+  }
+
+  return result;
+}
+//6
+function ChessBoard(size) {
+  const board = [];
+
+  for (let i = 0; i < size; i++) {
+    const row = [];
+
+    for (let j = 0; j < size; j++) {
+      const cell = (i + j) % 2 === 0 ? '  ' : '  ';
+      row.push(cell);
+    }
+
+    board.push(row.join(''));
+  }
+
+  console.log(board.join('\n'));
+}
