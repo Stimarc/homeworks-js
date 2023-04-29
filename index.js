bfb2eaa05a17115ca6dc70305b86b82af99c8874
Stimarc <rikudo7474@gmail.net>
2023-04-29 12:42:12 +0300

    finish him

--- index.js
+++ index.js
@@ -51,4 +51,70 @@ console.log(result);
 
 printObjectProperties(userWebsite[0]);
 //4
+const ElectronicStore = {
+  name: "StimShop",
+  address: "вул. Мостицька, 10, Київ, Україна",
+  products: [
+    {
+      brand: "Ноутбук Lenovo Legion 5 pro",
+      price: 65000,
+      description: "Екран 17.3 дюймів, Intel Core i7, 16 ГБ ОЗУ, 1000 ГБ HDD+256 ГБ SSD",
+    },
+    {
+      brand: "Смартфон Samsung Galaxy S 21 FE",
+      price: 21000,
+      description: "Екран 6.4 дюймів, 8 ГБ ОЗУ, 512 ГБ пам'яті, камера 50 МП",
+    },
+    {
+      brand: "Телевізор Samsung 55UN73006LA",
+      price: 35000,
+      description: "Екран 55 дюймів, 4K Ultra HD, Smart TV, Wi-Fi, Bluetooth",
+    },
+  ],
+};
 
+function sortByPriceDescending(store) {
+  store.products.sort((a, b) => b.price - a.price);
+  return store.products;
+}
+
+const sortedProducts = sortByPriceDescending(ElectronicStore);
+console.log(sortedProducts);
+//5
+const mistExpress = {
+  address: "вул. Проспект Правди, 10, Київ, Україна",
+  workingHours: {
+    monday: { open: "09:00", close: "18:00" },
+    tuesday: { open: "09:00", close: "18:00" },
+    wednesday: { open: "09:00", close: "18:00" },
+    thursday: { open: "09:00", close: "18:00" },
+    friday: { open: "09:00", close: "18:00" },
+    saturday: { open: "10:00", close: "16:00" },
+    sunday: { open: "closed", close: "closed" },
+  },
+  packages: [
+    { id: 1, sender: "Alex", recipient: "Lyuda", status: "delivered" },
+    { id: 2, sender: "Cezar", recipient: "Masha", status: "in transit" },
+    { id: 3, sender: "Jylia", recipient: "Igor", status: "pending" },
+  ],
+};
+
+function isMistExpressOpen() {
+  const today = new Date();
+  const currentDay = today.toLocaleDateString("en-US", { weekday: "lowercase" });
+  const currentTime = today.toLocaleTimeString("en-US", { hour12: false });
+
+  const openingTime = mistExpress.workingHours[currentDay].open;
+  const closingTime = mistExpress.workingHours[currentDay].close;
+
+  if (openingTime === "closed" || closingTime === "closed") {
+    return false;
+  } else if (currentTime >= openingTime && currentTime <= closingTime) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isMistExpressOpen()); 
+console.log(isMistEXPRESSOpen()); 
