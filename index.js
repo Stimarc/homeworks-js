d34ee784211c7768d052729ccda6e56ba12879aa
Stimarc <rikudo7474@gmail.net>
2023-04-30 21:51:15 +0300

    update  21:30

--- index.js
+++ index.js
@@ -1,62 +1,68 @@
-// 1.
+//setTimeout:
+//1 
+// function printAfter() {
+//   setTimeout(function() {
+//     console.log("Рядок, що виводиться через 5 секунд");
+//   }, 5000); 
+// }
+// printAfter();
+//2
+// const serverData = [
+//   {name:"Sam",id:1},
+//   {name:"Alex",id:2},
+//   {name:"Jeff",id:3}
+// ];
 
-let admin, name;
+// function fetchData() {
+//   setTimeout(() => {
+//     console.log(serverData);
 
-name = "Джон";
+//   }, 10000);
+// }
 
-admin = name;
-
-alert(admin);
-
-// 2.
-
-const planetName = "Ерта";
-
-console.info("Название планеты:", planetName);
-
-// 3.
-let variable1 = prompt("Введите значения 1:");
-let variable2 = prompt("Введите значения 2:");
-let variable3 = prompt("Введите значения 3:");
-
-console.warn(variable1);
-console.warn(variable2);
-console.warn(variable3);
-
-// 4.
-
-const Name = prompt("Как вас зовут?");
-const yearOfBirth = prompt("Сколько вам лет?");
+// fetchData();
 
 
-const yearOfBirthNumber = parseInt(yearOfBirth);
-if (isNaN(yearOfBirthNumber)) {
-  console.log("Вы ввели неправильную дату рождения.");
+//setInterval:
+//1
+// function countdown() {
+//   const now = new Date();
+//   const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
+//   const remainingTime = endOfDay - now;
+//   const remainingHours = Math.floor(remainingTime / (1000 * 60 * 60))
+//   const remainingMinutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+//   const remainingSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+//   console.log(`До кінця дня залишилось ${remainingHours} годин ${remainingMinutes} хвилин ${remainingSeconds} секунд`);
+// }
+
+// setInterval(countdown, 5000);
+
+// //Date:
+// //1
+// function getCurrentTime() {
+//   const now = new Date();
+//   const hours = now.getHours().toString().padStart(2, '0');
+//   const minutes = now.getMinutes().toString().padStart(2, '0');
+//   const seconds = now.getSeconds().toString().padStart(2, '0');
+//   return `${hours}:${minutes}:${seconds}`;
+// }
+// //2
+// function getCurrentMonth(){
+//   const now = new Date();
+//   const day = now.getDate().toString().padStart(2, '0');
+//   const month = (now.getMonth() + 1).toString().padStart(2, '0');
+//   const year = now.getFullYear().toString();
+//   return `${day}:${month}:${year}`;
+// }
+// //3
+// function daysToNewYear() {
+//   const today = new Date();
+//   const newYear = new Date(today.getFullYear() + 1, 0, 1);
+//   const diffTime = newYear - today;
+//   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+//   return diffDays;
+// }
+
+
  
-  throw new Error("Ошибка: неправильный год рождения.");
-}
-
-
-const currentYear = new Date().getFullYear();
-const age = currentYear - yearOfBirthNumber;
-
-
-if (age >= 18) {
-  console.log(`${Name}, вам ${age} лет. Ви можете гулять!`);
-} else {
-  console.log(`${Name}, к сожелению, ви еще не достигли совершенолетия. Гулять запрещено!`);
-}
-
-
-// .5
-
-const input = prompt("Введите число:");
-const number = Number(input);
-
-if (number >= 10 && number <= 1100) {
-  console.log(`${number} входит в диапазон от 10 до 1100.`);
-} else {
-  console.log(`${number} не входит в диапазон от 10 до 1100.`);
-}
-
 
