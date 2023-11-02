d2c30a92684dabafa3dd8451e82253bdbd02ef5c
Stimarc <124087165+Stimarc@users.noreply.github.com>
2023-11-02 20:48:22 +0200

    final project

--- client-bank/src/components/contacts.js
+++ client-bank/src/components/contacts.js
@@ -1,32 +1,33 @@
 export const contacts = () => {
-    return (
-      `
-      <div class="contact-page">
-        <h1>Contact Us</h1>
-        <form>
-          <label for="name">Name:</label>
-          <input type="text" id="name" name="name" placeholder="Your Name" required>
-  
-          <label for="email">Email:</label>
-          <input type="email" id="email" name="email" placeholder="Your Email" required>
+  return (
+    `
+    <div class="contact-page">
+      <h1 class="contact-heading">Contact Us</h1>
+      <form class="contact-form">
+        <label for="name" class="contact-label">Name:</label>
+        <input type="text" id="name" name="name" class="contact-input" placeholder="Your Name" required>
 
-          <label for="phone">Number Phone:</label>
-          <input type="number"  name="number" placeholder="Your Number" required>
-  
-          <label for="subject">Subject:</label>
-          <select id="subject" name="subject">
-            <option value="general">General Inquiry</option>
-            <option value="support">Customer Support</option>
-            <option value="feedback">Feedback</option>
-          </select>
-  
-          <label for="message">Message:</label>
-          <textarea id="message" name="message" placeholder="Your Message" rows="4" required></textarea>
-  
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-      `
-    );
-  };
+        <label for="email" class="contact-label">Email:</label>
+        <input type="email" id="email" name="email" class="contact-input" placeholder="Your Email" required>
+
+        <label for="phone" class="contact-label">Phone Number:</label>
+        <input type="tel" id="phone" name="phone" class="contact-input" placeholder="Your Phone Number" required>
+
+        <label for="subject" class="contact-label">Subject:</label>
+        <select id="subject" name="subject" class="contact-input">
+          <option value="general">General Inquiry</option>
+          <option value="support">Customer Support</option>
+          <option value="feedback">Feedback</option>
+        </select>
+
+        <label for="message" class="contact-label">Message:</label>
+        <textarea id="message" name="message" class="contact-input" placeholder="Your Message" rows="4" required></textarea>
+
+        <button type="submit" class="contact-button">Submit</button>
+      </form>
+    </div>
+    `
+  );
+};
+
   
